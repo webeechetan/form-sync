a4f0bc05cdc20d4728e265b53d53840dd62ee50e
Chetan Singh <chetan.singh@webeesocial.com>
2024-08-03 21:29:16 +0530

    added script tag

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,31 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<h1>
-    hello {{  auth()->user()->name}}
-</h1>
+<div class="row">
+    <div class="col-xxl-12 mb-12 order-0">
+      <div class="card">
+        <div class="card-header">
+           <input type="text" class="form-control script-tag" value="<script src='{{ env('APP_URL') }}/js/save-data.js'></script>" name="" id="">
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                Copy the script tag and paste it in your website to track the data.
+            </p>
+        </div>
+      </div>
+    </div>
+</div>
 @endsection
+
+@push('styles')
+@endpush
+
+@push('scripts')
+    <script>
+        $(document).ready(function(){
+            $('.script-tag').click(function(){
+                $(this).select();
+            });
+        });
+    </script>
+@endpush
